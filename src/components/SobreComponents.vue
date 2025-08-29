1c22a0dbb00da82fc46e666c9bc8fc9fa9e68f9c
Gianlucagadotti <gianlucagadotti@gmail.com>
2025-08-29 11:24:35 -0300

    FEAT:  #4 Nosso obejtivo feito pronto para colocar os textos

--- src/components/SobreComponents.vue
+++ src/components/SobreComponents.vue
@@ -36,6 +36,8 @@ const integrantes = [
 
      <div class="nosso">
       <h2>NOSSO OBJETIVO</h2>
+
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard  dummy text ever since the 1500s, when an unknown printer took a galley  of type and scrambled it to make a type specimen book. It has survived  not only five centuries, but also the leap into electronic typesetting,  remaining essentially unchanged. It was popularised in the 1960s with  the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker  including versions of Lorem Ipsum.</p>
      </div>
 
 
@@ -67,8 +69,8 @@ h1{
 div.sobre{
   text-align: center;
 
-  margin: 0;
-  height: 100vh;
+
+   height: auto;
   background-color: #215567;
 }
 ul.integrante {
@@ -91,19 +93,39 @@ ul.integrante li {
 ul.integrante li img {
   width: 100%;
   height: auto;
-
-
   border: 5px solid #8EAFB7;
 }
 div.nosso{
   background-color: #8EAFB7;
+  display: inline-block;
+  width: 60%;
+  border-radius: 4vw;
+  margin-top: 2vw;
+  margin-bottom: 5vw;
+
+
+
+}
+div.nosso h2{
+  background-color: #215567;
   color: white;
   font-size: 2.5rem;
-  display: inline-block;
-  padding: 1vw 4vw;
-  margin-top: 5vw;
-  border-top-left-radius: 4vw;
-  border-top-right-radius: 4vw;
+
+
+  margin: 0.7vw;
+  padding: 1vw 0;
+  border-top-left-radius: 3.3vw;
+  border-top-right-radius: 3.3vw;
+
+}
+div.nosso p{
+
+  padding: 2vw 4vw;
+  font-size: 1.5rem;
+  color: #215567;
+  text-align: justify;
+  font-family: 'Ade', serif;
+
 
 }
 
