56295f74a26475ea8c37769ce27e5c279cb557de
Joaobachmann <joaobachmannalun@gmail.com>
2025-08-31 12:40:23 -0300

    FEAT #9: criação e componetização do mapa. E alterações feitas na HeaderComponent.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
-
 <script setup>
-import HeaderComponent from './components/HeaderComponent.vue';
-</script>
-    <template>
-        <header>
-          <HeaderComponent/>
-        </header>
-        <main>
+import { ref } from 'vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import MapsComponent from './components/MapsComponent.vue'
 
-        </main>
-    </template>
+const activeSection = ref(null) // não começa no mapa
+
+function setSection(sec) {
+  activeSection.value = sec
+}
+</script>
+
+<template>
+  <div class="pagina">
+    <header>
+      <HeaderComponent :setSection="setSection" />
+    </header>
+    <main>
+      <section v-if="activeSection === 'mapa'">
+        <MapsComponent />
+      </section>
+
+      <section v-else-if="activeSection === 'cadastro'">
+      </section>
+
+      <section v-else-if="activeSection === 'reservar'">
+      </section>
+
+      <section v-else-if="activeSection === 'sobre'">
+      </section>
+    </main>
+  </div>
+</template>
 
 <style scoped>
 
