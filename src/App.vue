cbe0c202d504f76c5c446ba0c090d06fc7b17d58
Luizinx-1412 <luizinrobertomelocorreajunior@gmail.com>
2025-09-02 16:48:27 -0300

    FEAT: #8 CRIAÇÃO BOLHAS DO BACKGROUND

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup>
+<script>
 
 </script>
 
 <template>
-  <div class="geral">
+   
+<div class="geral">
+
+    
+ 
     <h1>RESERVE AGORA
     </h1>
 
@@ -30,31 +34,45 @@
                 <label for="pontosDistribuicao"></label>
                 <input type="text"  placeholder="Pontos de Distribuição:" class="pontos" name="pontosDistribuição">
             </div>
+            <div>
+              <img src="" alt="">
+            </div>
               <div>
                 <button type="submit">Confimar reserva</button>
             </div>
     </form>
-    
+    <div class="bolha bolha1"></div>
+  <div class="bolha bolha2"></div>
+  <div class="bolha bolha3"></div>
+  <div class="bolha bolha4"></div>
+  <div class="bolha bolha5"></div>
+
   </main>
-</div>
+  </div>
+
+
 </template>
 
 <style scoped>
 
 div.geral{
     height: 100vh;
-    background: linear-gradient(to bottom, #052837, #80A8BE);
+    background-color: #215567;
     
 }
 h1{
+  position: relative;
   text-align: center;
   color: white;
   font-size: 5vw;
+  z-index: 2;
 }
 form{
+  position: relative;
   text-align: center;
   border: none;
   outline: none;
+  z-index: 2;
 }
 .nome{
   margin: 3vw 0 1vw 0;
@@ -98,11 +116,60 @@ button{
   padding: 10px;
   border: 1px solid white;
   border-radius: 20px;
-  background: transparent;
   color: white;
   cursor: pointer;
   font-size: 1rem;
   transition: 0.3s;
+   background: rgba(255, 255, 255, 0.2);
 }
+.bolha1{
+      position: absolute;
+      width: 350px;
+      height: 350px;
+      background-image: linear-gradient( 320deg, #215567, #BED6DA);
+      border-radius: 50%;
+      z-index: 1;
+      
+}
+    .bolha2 {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      background-image: linear-gradient( 320deg, #215567, #BED6DA);
+      border-radius: 50%;
+       z-index: 1; 
+      
+    
+    }
+    .bolha3 {
+     position: absolute;
+      background-image: linear-gradient( 320deg, #215567, #BED6DA);
+      border-radius: 50%;
+       z-index: 1; 
+      
+    }
+    .bolha4 {
+      position: absolute;
+      width: 100px;
+      height: 100px;
+      background-image: linear-gradient( 320deg, #215567, #BED6DA);
+      border-radius: 50%;
+      
+    }
+    .bolha5 {
+      position: absolute;
+      width: 100px;
+      height: 100px;
+      background-image: linear-gradient( 320deg, #215567, #BED6DA);
+      border-radius: 50%;
+      
+    }
+
+    /* Exemplos de bolhas em posições diferentes */
+    .bolha1 { bottom: -5%; left: -5%; }
+    .bolha2 { top: -5%; left: 46%; width: 370px; height: 370px;}
+    .bolha3 { top: 70%; left: 70%; width: 200px; height: 200px; }
+    .bolha4 { top: 30%; left: 84%; width: 300px; height: 300px; }
+    .bolha5 { top: 60%; left: 30%; width: 20px; height: 20px; }
 
 </style>
